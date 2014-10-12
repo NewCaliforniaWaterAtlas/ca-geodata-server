f03a52ae03d8b4b5f174f8f9265dfeeb0a6c5dcf
videmsky <stuffaccounts@gmail.com>
2014-10-11 17:55:33 -0700

    added geojson routes

---
+++ routes/geojson/groutes.js
@@ -0,0 +1,278 @@
+var pg = require('pg');
+var geojson = require('./helpers/geojson');
+var jsonp = require('./helpers/jsonp');
+
+module.exports = function (app) {
+
+  /* enable CORS */
+  app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+  app.get('/counties/:id', function (req, res, next) {
+
+    var client = new pg.Client(app.conString);
+    client.connect();
+    var crsobj = {"type": "name","properties": {"name": "urn:ogc:def:crs:EPSG:6.3:4326"}};
+    var idformat = "'" + req.params.id + "'";
+    var query = client.query("SELECT ST_AsGeoJson(geom) AS geojson FROM counties_ca WHERE LOWER(name) = LOWER(" + idformat + ");" );
+    var retval = "no data";
+    query.on('row', function(result) {
+      client.end();
+      if (!result) {
+        return res.send('No data found');
+      } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send({type: "feature", crs: crsobj, geometry: JSON.parse(result.geojson), properties:{"iso": req.params.id, "representation": "boundary"}});
+      }
+    }); 
+
+  });
+
+
+
+  /* feature retrieval */
+
+  /**
+   * retrieve all features (this could be really slow and is probably not what you really want to do)
+   */
+  app.get('/vector/:schema/:table/:geom', function (req, res, next) {
+    
+    var client = new pg.Client(app.conString);
+    var geom = req.params.geom.toLowerCase();
+    if ((geom != "features") && (geom != "geometry")) {
+      res.status(404).send("Resource '" + geom + "' not found");
+      return;
+    }
+    
+    var schemaname = req.params.schema;
+    var tablename = req.params.table;
+    var fullname = schemaname + "." + tablename;
+    client.connect();
+    var crsobj = {
+      "type" : "name",
+      "properties" : {
+        "name" : "urn:ogc:def:crs:EPSG:6.3:4326"
+      }
+    };
+    
+    var idformat = "'" + req.params.id + "'";
+    idformat = idformat.toUpperCase();
+    var spatialcol = "";
+    
+    var meta = client.query("select * from geometry_columns where f_table_name = '" + tablename + "' and f_table_schema = '" + schemaname + "';");
+    
+    meta.on('row', function (row) {
+      var query;
+      var coll;
+      spatialcol = row.f_geometry_column;
+      if (geom == "features") {
+        query = client.query("select st_asgeojson(st_transform(" + spatialcol + ",4326)) as geojson, * from " + fullname + ";");
+        coll = {
+          type : "FeatureCollection",
+          features : []
+        };
+      } else if (geom == "geometry") {
+        query = client.query("select st_asgeojson(st_transform(" + spatialcol + ",4326)) as geojson from " + fullname + ";");
+        coll = {
+          type : "GeometryCollection",
+          geometries : []
+        };
+      }
+      
+      query.on('row', function (result) {
+        if (!result) {
+          return res.send('No data found');
+        } else {
+          if (geom == "features") {
+            coll.features.push(geojson.getFeatureResult(result, spatialcol));
+          } else if (geom == "geometry") {
+            var shape = JSON.parse(result.geojson);
+            //shape.crs = crsobj;
+            coll.geometries.push(shape);
+          }
+        }
+      });
+
+      query.on('end', function (err, result) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(jsonp.getJsonP(req.query.callback, coll));
+      });
+
+      query.on('error', function (error) {
+        //handle the error
+        //res.status(500).send(error);
+        next();
+      });
+    });
+  });
+
+  /**
+   * retrieve all features that intersect the input GeoJSON geometry
+   */
+  // app.post('/vector/:schema/:table/:geom/intersect', function (req, res, next) {
+  //   //console.log(JSON.stringify(req.body));
+  //   var queryshape = JSON.stringify(req.body);
+  //   //res.status(501).send('Intersect not implemented');
+  //   var geom = req.params.geom.toLowerCase();
+  //   if ((geom != "features") && (geom != "geometry")) {
+  //     res.status(404).send("Resource '" + geom + "' not found");
+  //     return;
+  //   }
+    
+  //   var client = new pg.Client(app.conString);
+  //   var schemaname = req.params.schema;
+  //   var tablename = req.params.table;
+  //   var fullname = schemaname + "." + tablename;
+  //   client.connect();
+  //   var crsobj = {
+  //     "type" : "name",
+  //     "properties" : {
+  //       "name" : "urn:ogc:def:crs:EPSG:6.3:4326"
+  //     }
+  //   };
+    
+  //   var idformat = "'" + req.params.id + "'";
+  //   idformat = idformat.toUpperCase();
+  //   var spatialcol = "";
+    
+  //   var meta = client.query("select * from geometry_columns where f_table_name = '" + tablename + "' and f_table_schema = '" + schemaname + "';");
+    
+  //   meta.on('row', function (row) {
+      
+  //     var query;
+  //     var coll;
+  //     spatialcol = row.f_geometry_column;
+  //     if (geom == "features") {
+  //       query = client.query("select st_asgeojson(st_transform(" + spatialcol + ",4326)) as geojson, * from " + fullname + " where ST_INTERSECTS(" + spatialcol + ", ST_SetSRID(ST_GeomFromGeoJSON('" + queryshape + "'),4326));");
+  //       coll = {
+  //         type : "FeatureCollection",
+  //         features : []
+  //       };
+  //     } else if (geom == "geometry") {
+  //       query = client.query("select st_asgeojson(st_transform(" + spatialcol + ",4326)) as geojson from " + fullname + " where ST_INTERSECTS(" + spatialcol + ", ST_SetSRID(ST_GeomFromGeoJSON('" + queryshape + "'),4326));");
+  //       coll = {
+  //         type : "GeometryCollection",
+  //         geometries : []
+  //       };
+  //     }
+
+  //     query.on('row', function (result) {
+  //       var props = new Object;
+  //       if (!result) {
+  //         return res.send('No data found');
+  //       } else {
+  //         if (geom == "features") {
+  //           coll.features.push(geojson.getFeatureResult(result, spatialcol));
+  //         } else if (geom == "geometry") {
+  //           var shape = JSON.parse(result.geojson);
+  //           //shape.crs = crsobj;
+  //           coll.geometries.push(shape);
+  //         }
+  //       }
+  //     });
+
+  //     query.on('end', function (result) {
+  //       res.setHeader('Content-Type', 'application/json');
+  //       res.send(jsonp.getJsonP(req.query.callback, coll));
+
+  //     });
+  //     query.on('error', function (error) {
+  //       //handle the error
+  //       //res.status(500).send(error);
+  //       //next();
+  //     });
+
+  //   });
+  // });
+
+  /*  Schema inspection functions  */
+
+  /* fetch table schema */
+  // app.get('/vector/layer/:schema/:table/schema', function (req, res, next) {
+  //   var client = new pg.Client(app.conString);
+  //   var schemaname = req.params.schema;
+  //   var tablename = req.params.table;
+  //   var fullname = schemaname + "." + tablename;
+  //   var sql = "SELECT n.nspname as schemaname,c.relname as table_name,a.attname as column_name,format_type(a.atttypid, a.atttypmod) AS type,col_description(a.attrelid, a.attnum) as comments";
+  //   sql = sql + " FROM pg_class c INNER JOIN pg_namespace n ON c.relnamespace = n.oid LEFT JOIN pg_attribute a ON a.attrelid = c.oid";
+  //   sql = sql + " WHERE a.attnum > 0 and c.relname = '" + tablename + "' and n.nspname = '" + schemaname + "';";
+  //   var retval = {
+  //     schema : schemaname,
+  //     table : tablename,
+  //     columns : []
+  //   };
+  //   client.connect();
+  //   var query = client.query(sql);
+  //   query.on('row', function (result) {
+  //     var props = new Object;
+  //     if (!result) {
+  //       return res.send('No data found');
+  //     } else {
+  //       retval.columns.push({
+  //         column : result.column_name,
+  //         dataType : result.type,
+  //         description : result.comments
+  //       });
+  //     }
+  //   });
+
+  //   query.on('end', function (result) {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.send(jsonp.getJsonP(req.query.callback, retval));
+  //     //
+  //   });
+
+  // });
+
+  // /* fetch table schema (not compatible with PostGIS versions prior to 1.5) */
+  // app.get('/vector/layers/:geotype', function (req, res, next) {
+    
+  //   var client = new pg.Client(app.conString);
+  //   var sql = "SELECT 'geometry' as geotype, * FROM geometry_columns;";
+  //   if (req.params.geotype.toLowerCase() == "geography") {
+  //     sql = "SELECT 'geography' as geotype, * FROM geography_columns;";
+  //   } else if (req.params.geotype.toLowerCase() == "all") {
+  //     sql = "SELECT 'geometry' AS geotype, * FROM geometry_columns UNION SELECT 'geography' as geotype, * FROM geography_columns;";
+  //   }
+  //   var retval = [];
+  //   client.connect();
+  //   var query = client.query(sql);
+  //   query.on('row', function (result) {
+  //     var props = new Object;
+  //     if (!result) {
+  //       return res.send('No data found');
+  //     } else {
+  //       retval.push(getRow(result, req.params.geotype.toLowerCase()));
+  //     }
+  //   });
+
+  //   query.on('end', function (result) {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.send(jsonp.getJsonP(req.query.callback, retval));
+  //   });
+
+  // });
+
+  // function getRow(result, geomtype) {
+  //   var retval = {
+  //     geoType : result.geotype,
+  //     database : result.f_table_catalog,
+  //     schema : result.f_table_schema,
+  //     table : result.f_table_name,
+  //     spatialColumn : null,
+  //     dimension : result.coord_dimension,
+  //     srid : result.srid,
+  //     spatialType : result.type
+  //   };
+  //   if ((geomtype == "geometry") || (geomtype == "all")) {
+  //     retval.spatialColumn = result.f_geometry_column;
+  //   } else {
+  //     retval.spatialColumn = result.f_geography_column;
+  //   }
+  //   return retval;
+  // }
+} // end module.exports
+
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-topoJSONStubs = require('./routes/topojson/stubs.js');
+var topoJSONEnds = require('./routes/topojson/troutes.js');
+var credentials = require('./credentials');
+var express = require('express');
 
-var port = process.env.PORT || 3000;
+var app = express();
+app.use(express.bodyParser());
+app.use(app.router);
+app.conString = "postgres://" + credentials.user + ":" + credentials.passwd + "@ncwageoservice.ccts9i164ms9.us-east-1.rds.amazonaws.com/ncwadb";
 
-topoJSONStubs.server.listen(port);
-console.log("App listening on port " + port);
+var tport = process.env.PORT || 3000;
+var gport = process.env.PORT || 8000;
+
+process.on('uncaughtException', function (error) {
+  console.log(error.stack);
+});
+
+require('./routes/geojson/groutes.js')(app);
+
+app.listen(gport);
+console.log("App listening on port " + gport);
+
+topoJSONEnds.server.listen(tport);
+console.log("App listening on port " + tport);
